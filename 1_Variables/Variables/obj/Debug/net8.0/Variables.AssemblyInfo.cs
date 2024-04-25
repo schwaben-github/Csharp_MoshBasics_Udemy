cf84410b5eefb6896fa6022f32b90d79dc62efe6
Tibor W. <schwaben-github@users.noreply.github.com>
2024-04-25 11:50:19 +0200

    Initial commit

--- 1_Variables/Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
+++ 1_Variables/Variables/obj/Debug/net8.0/Variables.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dee7598f37dc33c49110afc83b08810d578806b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
