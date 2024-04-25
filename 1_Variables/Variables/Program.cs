d258840edd878e957e0a7c1b1ec8ff0ffe470424
Tibor W. <schwaben-github@users.noreply.github.com>
2024-04-25 15:17:06 +0200

    Update excercise Variables

--- 1_Variables/Variables/Program.cs
+++ 1_Variables/Variables/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            byte number = 2;
+            var number = 2;
+            // byte number = 2;
             int count = 10;
             float totalPrice = 20.95f;
             char character = 'A';
@@ -17,6 +18,62 @@
             Console.WriteLine(character);
             Console.WriteLine(firstName);
             Console.WriteLine(isWorking);
+
+            // Ctrl + X to cut the line
+            // View -> Object Browser
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            const float Pi = 3.14f;
+
+
+            // Implicit Type Conversion
+            byte b = 1; // 00000001
+            int i = b; // 00000000 00000000 00000000 00000001
+
+            // Explicit Type Conversion
+            int j = 1;
+            byte c = (byte)j; // casting!
+
+            float f = 1.0f;
+            int k = (int)f;  // casting!
+
+            string s = "1";
+            int l = Convert.ToInt32(s);
+            int m = int.Parse(s);
+
+            int n = 1; // chance of data loss if the number is too big!
+            byte o = (byte)n;
+            Console.WriteLine(o);
+
+            // Non-compatible types
+            var number1 = "1234";
+            int number2 = Convert.ToInt32(number1);
+            Console.WriteLine(number2);
+
+            // Handling exceptions
+            try
+            {
+                var number3 = "1234";
+                byte number4 = Convert.ToByte(number3);
+                Console.WriteLine(number4);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+
+            try
+            {
+                string str = "true";
+                bool b1 = Convert.ToBoolean(str);
+                Console.WriteLine(b1);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The string could not be converted to a boolean.");
+            }
         }
     }
 }
