e4287cc3c5f00bf2ec5fbd8092f574ff4b908996
Tibor W. <schwaben-github@users.noreply.github.com>
2024-04-29 11:30:37 +0200

    Update apps

--- Loops/RandomClass/Program.cs
+++ Loops/RandomClass/Program.cs
@@ -1,12 +1,28 @@
 ﻿var random = new Random();
 
-const int passwordLenght = 10;
-var buffer = new char[passwordLenght];
+const int passwordLength = 10;
+var buffer = new char[passwordLength];
 
-for (int i = 0; i < passwordLenght; i++)
+for (int i = 0; i < passwordLength; i++)
 {
-    buffer[i] = (char)('a' + random.Next(0, 26));
+    int randomValue = random.Next(0, 62);
+    if (randomValue < 10)
+    {
+        // Generate a random digit
+        buffer[i] = (char)('0' + randomValue);
+    }
+    else if (randomValue < 36)
+    {
+        // Generate a random uppercase letter
+        buffer[i] = (char)('A' + randomValue - 10);
+    }
+    else
+    {
+        // Generate a random lowercase letter
+        buffer[i] = (char)('a' + randomValue - 36);
+    }
+}
 
-    var password = new string(buffer);
+var password = new string(buffer);
 
-    Console.WriteLine(password);
+Console.WriteLine(password);
